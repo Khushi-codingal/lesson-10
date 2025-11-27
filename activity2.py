6170453e42cede800aa7878fcf0838320fab1373
Khushi-codingal <rashmiabc@gmail.com>
2025-11-27 17:41:54 +0530

    strings

---
+++ activity2.py
@@ -0,0 +1,6 @@
+string = input("please enter your string:")
+string2 = (" ")
+for i in string:
+    string2 = i+string2
+print("\nthe original string is:", string)
+print("the reversed string is:", string2)
